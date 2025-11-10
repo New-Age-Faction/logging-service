a7eb9e5d4aa05e7f730cc9f2278df9118fc7b13d
LYAM <cous5830@gmail.com>
2025-11-10 21:15:26 +0000

    Start of live chat routes

---
+++ src/routes/liveChat.routes.ts
@@ -0,0 +1,49 @@
+/*
+  Routes all the sub routes of the live chat logging to their individual
+  endpoints. Essentially linking the controller to the endpoints.
+*/
+
+// - Imports ------------------------------------------------------------------
+import { Router } from "express";
+import { logger } from "../utils/logger";
+import path from "path";
+import fs from "fs";
+import { makeServiceLogController } from "../controllers/serviceLogs.controller";
+
+// - Routing ------------------------------------------------------------------
+// TODO: CLEAN UP
+const router = Router();
+
+logger.info("Managing Live chat endpoints...");
+logger.debug("Creating path to live chat endpoint strategies");
+const endpointsDir = path.join(__dirname, "../strategies/liveChat");
+logger.debug(`Strategies are found in: ${endpointsDir}`);
+
+logger.debug(`Gathering endpoint strategy files`);
+const files = fs
+  .readdirSync(endpointsDir)
+  .filter((f) => f.endsWith(".strategy.ts"));
+
+for (const file of files) {
+  logger.debug(`\tManaging: ${file}`);
+  const modulePath = path.join(endpointsDir, file);
+  const module = import(modulePath).then((module) => {
+    logger.info(`Finished importing ${file}. Parsing import contents`);
+
+    // Find the exported class (assume first exported class is the endpoint)
+    const EndpointClass = Object.values(module).find(
+      (v) => typeof v === "function" && v.toString().startsWith("class "),
+    ) as new () => any;
+
+    if (!EndpointClass) {
+      logger.warning(`\tSkipping. No valid endpoint class found.`);
+    }
+
+    const endpoint = new EndpointClass();
+    const controller = makeServiceLogController(endpoint);
+    router.post(`/${endpoint.name}`, controller);
+  });
+}
+
+logger.info("Live chat router configured");
+export default router;
